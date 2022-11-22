9b27e98c684970d43f2b65492bf0bbb9b841c9f5
Suraj Kumar <51703862+1nf1n1ty1999@users.noreply.github.com>
2022-11-22 13:03:16 +0530

    lastcal

---
+++ app/src/main/java/com/example/lastcal/Success.kt
@@ -0,0 +1,26 @@
+package com.example.lastcal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.example.lastcal.models.RestaurentModel
+import kotlinx.android.synthetic.main.activity_success.*
+
+class Success : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_success)
+
+        val restaurantModel: RestaurentModel? = intent.getParcelableExtra("RestaurantModel")
+        val actionbar: ActionBar? = supportActionBar
+        actionbar?.setTitle(restaurantModel?.name)
+        actionbar?.setSubtitle(restaurantModel?.address)
+        actionbar?.setDisplayHomeAsUpEnabled(false)
+
+        buttonDone.setOnClickListener {
+
+            setResult(RESULT_OK)
+            finish()
+        }
+    }
+}
